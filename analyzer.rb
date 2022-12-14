7dec5511af5ecca0db5b3b0a1862949d9cb248b1
Brett Seifried <seifriedbrett@gmail.com>
2022-12-13 19:49:53 -0800

    user inputs. first analyzer project

---
+++ analyzer.rb
@@ -0,0 +1,19 @@
+# First Project
+# input first name
+# inputs second name
+# return full name
+# return reversed name
+# give length
+
+# Get first name
+
+puts "What is your first name?"
+first_name = gets.chomp
+puts "What is your last name?"
+last_name = gets.chomp
+
+full_name = first_name + last_name
+
+puts "Your full name is #{first_name} #{last_name}"
+puts "Your full name reveresed is #{last_name.reverse} #{first_name.reverse}"
+puts "Your name has #{full_name.length} characters in it"
---
+++ userInput.rb
@@ -0,0 +1,29 @@
+# # interact to get use first name
+# puts "What is your first name?"
+
+# # user inputs (try in terminal) with gets.chomp
+# # gets.chomp gets a string
+# first_name = gets.chomp
+
+# puts "Thank you, you said your first name is #{first_name}"
+
+#returns
+    #What is your first name?
+    # user input
+    # Thank you, you said your name is (what they entered)
+
+# Everything above commented out -------------
+
+# # to get a number
+# puts "Enter a number to multiply by 2"
+# input = gets.chomp
+# # puts input * 2 
+# # if user inputs 10. will return 1010
+# # gets.chomp takes user input and puts into a STRING ******
+# # .to_i turns that string into a number
+# puts input.to_i * 2
+# # user inputs 10 => "10"
+# # input.to_i => 10
+# # returns result of 20 = 2 * 10
+
+# Everything above commented out -------------

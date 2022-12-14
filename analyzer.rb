ccc1665fadcff4dc69247bba1d35aecd55ef73ff
Brett Seifried <seifriedbrett@gmail.com>
2022-12-13 19:52:01 -0800

    removed full_name from analyzer.rb

--- analyzer.rb
+++ analyzer.rb
@@ -12,8 +12,6 @@ first_name = gets.chomp
 puts "What is your last name?"
 last_name = gets.chomp
 
-full_name = first_name + last_name
-
 puts "Your full name is #{first_name} #{last_name}"
 puts "Your full name reveresed is #{last_name.reverse} #{first_name.reverse}"
 puts "Your name has #{first_name.length + last_name.length} characters in it"

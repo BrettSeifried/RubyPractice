ab14900be367146df20aee85ac25c785b88278ed
Brett Seifried <seifriedbrett@gmail.com>
2022-12-15 18:56:54 -0800

    created methods for calculator

--- calculator.rb
+++ calculator.rb
@@ -20,3 +20,38 @@ puts "The first number added to the second number is #{num_1.to_f + num_2.to_f}"
 
 # Modulus Operator shows the operator
 puts "The modular of the first number by the second number is #{num_1.to_f % num_2.to_f}"
+
+
+# ------------------------
+# Methods
+
+def multiply(first_num, second_num)
+    # the last evaluated statement/expression in a method
+    # Return is auto implied
+    # don't need to conclude return
+    first_num.to_f * second_num.to_f
+end
+def divide(first_num, second_num)
+    first_num.to_f / second_num.to_f
+end
+def subtract(first_num, second_num)
+    first_num.to_f - second_num.to_f
+end
+def add(first_num, second_num)
+    first_num.to_f + second_num.to_f
+end
+def mod(first_num, second_num)
+    first_num.to_f % second_num.to_f
+end
+
+# Will reutrn undefined method if applied above
+# def must be before the puts
+puts "Method first number times second number is #{multiply(num_1, num_2)}"
+# Method Divide
+puts "Method Divide #{divide(num_1 , num_2)}"
+# Method Subtracted
+puts "Method Subtract #{subtract(num_1, num_2)}"
+# Method Add
+puts "Method Addition #{add(num_1, num_2)}"
+# Method Modular
+puts "Method Modular is #{mod(num_1, num_2)}"
---
+++ comparison.rb
@@ -0,0 +1,39 @@
+# Comparison Operators
+    # Trying to comapre two items
+    # == Equal?
+        # a == b ?
+    # != not equal to
+    # > greater than
+    # < less than
+
+puts 10 == 9
+# Returns false
+
+puts 10 == 10
+# True
+
+puts 10 == "10".to_i
+# True
+# same for "10".to_f
+
+puts 10 != 9
+# True
+
+puts "hello" != "bye"
+# True
+
+puts 100 > 99
+# True
+
+puts 100 >= 99
+# True
+
+puts "" == " "
+# False
+
+puts 10 === "10".to_f
+# True
+
+puts 10.eql?(10.0)
+# False
+# .eql? compares types not the values

aaab6d6a08457276769dabbfe0cd3344a9ce5658
Brett Seifried <seifriedbrett@gmail.com>
2022-12-15 17:49:27 -0800

    changed integers to floats for better division

--- calculator.rb
+++ calculator.rb
@@ -8,15 +8,15 @@ num_2 = gets.chomp
 # puts "The first number multiplied by the second number is #{num_1 * num_2}"
 # This will return an error for strings
 # we want to keep the users raw input raw. so we dont' change it above
-puts "The first number multiplied by the second number is #{num_1.to_i * num_2.to_i}"
+puts "The first number multiplied by the second number is #{num_1.to_f * num_2.to_f}"
 
 # Homework
 # Division
-puts "The first number divided by the second number is #{num_1.to_i / num_2.to_i}"
+puts "The first number divided by the second number is #{num_1.to_f / num_2.to_f}"
 # Subtraction
-puts "The first number subtracted by the second number is #{num_1.to_i - num_2.to_i}"
+puts "The first number subtracted by the second number is #{num_1.to_f - num_2.to_f}"
 # Addition
-puts "The first number added to the second number is #{num_1.to_i + num_2.to_i}"
+puts "The first number added to the second number is #{num_1.to_f + num_2.to_f}"
 
 # Modulus Operator shows the operator
-puts "The modular of the first number by the second number is #{num_1.to_i % num_2.to_i}"
+puts "The modular of the first number by the second number is #{num_1.to_f % num_2.to_f}"

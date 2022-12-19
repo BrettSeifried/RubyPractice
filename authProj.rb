73b06f415625e5e3438e95ebebfb27cde003c9a8
Brett Seifried <seifriedbrett@gmail.com>
2022-12-19 11:25:08 -0800

    working method for first user

--- authProj.rb
+++ authProj.rb
@@ -1,10 +1,24 @@
 users = [
-    { username: 'brettford', password: 'password1 '},
-    { username: 'christina', password: 'password2 '},
-    { username: 'brooke', password: 'password3 '},
-    { username: 'archer', password: 'password4 '},
-    { username: 'rue', password: 'password5 '},
+    { username: "brettford", password: "password1"},
+    { username: "christina", password: "password2"},
+    { username: "brooke", password: "password3"},
+    { username: "archer", password: "password4"},
+    { username: "rue", password: "password5"},
 ]
+
+#6.2 Method
+    # 3 things I need to this method
+def auth_user(userVar, passVar, list_of_users)
+    list_of_users.each do |user_record|
+        #compares key/value from each row
+        if user_record[:username] == userVar && user_record[:password] == passVar
+            # return exits the method
+            return user_record
+        else
+            return "Credentials were not correct"
+        end
+    end
+end
 
 puts "Welcome to the Auth project"
 25.times { print "-" }
@@ -12,7 +26,43 @@ puts
 puts "This program will take an input from the user and comapre passwords"
 puts "If the password is correct, you will get back the user object"
 
-print "Username: "
-user_name = gets.chomp
-print "Password: "
-user_pass = gets.chomp
+attempts = 1
+
+# 1. keeps looking while it is true or a break condition is met
+while attempts < 4 
+# 4. user name
+    print "Username: "
+    user_name = gets.chomp.downcase
+
+    print "Password: "
+    user_pass = gets.chomp.downcase
+
+# 5. auth
+#     users.each do |user|
+#         #compares key/value from each row
+#         if user[:username] == user_name && user[:password] == user_pass
+#             puts user
+#             # gets out of the while look once complete
+#             break
+#         else
+#             puts "Credentials were not correct"
+#     end
+# end
+# 6. Refactored above witha  method
+    # call the method
+    auththentication = auth_user(user_name, user_pass, users)
+    # print something
+    puts auththentication
+# 3. give the user a way to break condition
+    puts "Press n to quit or any other key to continue: "
+    input = gets.chomp.downcase
+# breaks the loop if n is pushed
+    # if input == "n"
+    #     break
+    # end
+    # CONVERTED TO ONE LINE
+    break if input == "n"
+# 2. increments an attempt
+    attempts += 1
+
+end
